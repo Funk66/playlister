8f683750c57156162a6ba46c18f0c8821ccf4541
Guillermo Guirao Aguilar <contact@guillermoguiraoaguilar.com>
2020-01-05 18:29:34 +0100

    Added console script

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup
 
-
-setup(name='playlister',
-      version='0.1.0',
-      author='Guillermo Guirao Aguilar',
-      author_email='contact@guillermoguiraoaguilar.com',
-      py_modules=['playlister'],
-      description='Spotify playlist generator',
-      url='https://github.com/Funk66/playlister',
-      license='MIT',
-      classifiers=['Programming Language :: Python :: 3.8'])
+setup(
+    name='playlister',
+    version='0.1.0',
+    author='Guillermo Guirao Aguilar',
+    author_email='contact@guillermoguiraoaguilar.com',
+    py_modules=['playlister'],
+    description='Spotify playlist generator',
+    url='https://github.com/Funk66/playlister',
+    license='MIT',
+    classifiers=['Programming Language :: Python :: 3.8'],
+    packages=['playlister'],
+    entry_points={
+        'console_scripts': ['playlister=playlister.client:update']
+    })
